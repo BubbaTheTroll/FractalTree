e266a0c6cb50722bdff57b95d505df68c009b2e6
alcai3 <alcai3@697-334-Combe.sf.k12.internal>
2015-01-29 10:13:22 -0800

    added stuff to index.html

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,4 +1,20 @@
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f; 
 private int smallestBranch = 60; 
 private double branchAngle1 = Math.random();
 private double branchAngle2 = Math.random(); 
@@ -18,13 +34,13 @@ public void draw()
 } 
 public void keyTyped(){
 	if(key == 'w'){
-		smallestBranch-=.2;
+		smallestBranch-=.2f;
 	}
 	if(key == 'a'){
-		branchAngle1+=.2;
+		branchAngle1+=.2f;
 	}
 	if(key == 'd'){
-		branchAngle1-=.2;
+		branchAngle1-=.2f;
 	}
 	redraw();
 }
@@ -56,3 +72,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
 
 	//your code here    
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
